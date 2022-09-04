957e502ae8cbb6d13c5fe2b9f11c25cc4f858927
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-09-04 16:08:04 +0300

    sum

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
+console.log(galleryItems);
+
+const imageItem = ({ original, preview, description }) => {
+  const linkEl = document.createElement("a");
+  linkEl.classList.add("gallery__link");
+  linkEl.href = `${original}`;
+
+  const imageEl = document.createElement("img");
+  imageEl.classList.add("gallery__image");
+  imageEl.src = `${preview}`;
+  imageEl.alt = `${description}`;
+  linkEl.append(imageEl);
+  return linkEl;
+};
+const imageCard = galleryItems.map(imageItem);
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.append(...imageCard);
+
+galleryEl.addEventListener("click", onImageClick);
+function onImageClick(event) {
+  blockStandartAction(event);
+}
+function blockStandartAction(event) {
+  event.preventDefault();
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
