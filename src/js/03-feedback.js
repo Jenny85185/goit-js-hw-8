16c66664af80d821bf6b51d9f52f7adeeec26482
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-09-05 14:02:09 +0300

    SUM

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,57 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
+const formEl = document.querySelector('.feedback-form');
+const textareaEl = document.querySelector('textarea');
 
-const formData = {};
-dataFromLocalStorage();
+const STORAGE_KEY = 'feedback-form-state';
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+let formData = {};
+
+formEl.addEventListener('input', throttle(onEmailMessageSave, 500));
+formEl.addEventListener('submit', onSubmit);
+
+populateInput();
+
+
+function onEmailMessageSave(evt) {
+  evt.preventDefault();
+
+ 
+  const emailInput = formEl.elements.email.value;
+  const messageInput = textareaEl.value;
+
+  formData = { email: emailInput, message: messageInput };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
-}
 
-function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+
+function populateInput() {
+  const saveInput = localStorage.getItem(STORAGE_KEY);
+
+  const parsSaveInput = JSON.parse(saveInput);
+
+  if (saveInput) {
+    textareaEl.value = parsSaveInput.message || '';
+    formEl.elements.email.value = parsSaveInput.email || '';
   }
-};
+
+  formData = parsSaveInput;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  submitInput = {
+    Message: textareaEl.value,
+    Email: formEl.elements.email.value,
+  };
+  console.log(submitInput);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  evt.currentTarget.reset();
+}
